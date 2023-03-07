c65948f8971257002a4c037dba845cce474c7ced
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-03-07 14:53:09 +0600

    based setup is done

---
+++ src/Services/themeSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { GET_LOCALSTORAGE_DATA } from "../config";
+const initialState = {
+  theme: GET_LOCALSTORAGE_DATA("dark-mode"),
+};
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    addTheme(state, action) {
+      if (action.type === "THEME") {
+        return state.theme = action.payload;
+      }
+    },
+  },
+});
+export const { addTheme } = themeSlice.actions;
+export default themeSlice.reducer;
